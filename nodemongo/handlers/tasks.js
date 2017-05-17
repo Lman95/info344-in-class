5802d14a64b7e49099d9dcc91071e52abc985419
Landon Young <young518@uw.edu>
2017-05-17 15:03:43 -0700

    Everything up to 5/17

--- nodemongo/handlers/tasks.js
+++ nodemongo/handlers/tasks.js
@@ -1,11 +1,65 @@
 "use strict";
 
 const express = require('express');
+const Task = require('../models/tasks/task.js');
 
 //export a function from this module 
 //that accepts a tasks store implementation
 module.exports = function(store) {
-    //TODO: create an express.Router()
-    //add handlers to it
-    //and return the configured Router
+    // create a new Mux
+    let router = express.Router();
+
+    router.get('/v1/tasks', async (req, res, next) => {
+        try {
+            let tasks = await store.getAll();
+            res.json(tasks);
+        } catch(err) {
+            next(err);
+        }
+
+        // store.getAll()
+        //     .then(tasks => {
+        //         res.json(tasks);
+        //     })
+        //     .catch(next);
+    });
+
+    router.post('/v1/tasks', async (req, res, next) => {
+        try {
+            let task = new Task(req.body);
+            let err = task.validate();
+            if (err) {
+                res.status(400).send(err.message);
+            } else {
+                let result = await store.insert(task);
+                res.json(task);
+            }
+        } catch(err) {
+            next(err);
+        }
+    });
+
+    router.patch('/v1/tasks/:taskID', async (req, res, next) => {
+        let taskID = req.params.taskID;
+        try {
+            let update = await store.setComplete(taskID, req.body.complete);
+            res.json(update);
+        } catch(err) {
+            next(err);
+        }
+
+    });
+
+    router.delete('/v1/tasks/:taskID', async (req, res, next) => {
+        let taskID = req.params.taskID;
+        try {
+            let result = await store.delete(taskID);
+            res.send(`deleted task ${taskID}`);
+        } catch(err) {
+            next(err);
+        }
+
+    });
+
+    return router;
 };
